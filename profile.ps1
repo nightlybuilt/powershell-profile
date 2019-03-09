b2ef0f72776b003c9b08c568129bc1a523bde741
nightlybuilt <hmorris@phluant.com>
2019-03-10 05:05:13 +0800

    reload did not work with Powershell Core 6

--- profile.ps1
+++ profile.ps1
@@ -23,27 +23,3 @@ Get-ChildItem "D:\wamp64\bin\mysql\mysql5.7.24\bin" -Filter mysql*.exe |
 Foreach-Object {
     Set-Alias -Name $_.BaseName -Value $_.FullName
 }
-
-# https://communary.net/2015/05/28/how-to-reload-the-powershell-console-session/
-function Invoke-PowerShell {
-    powershell -nologo
-    Invoke-PowerShell
-}
-
-function Restart-PowerShell {
-    if ($host.Name -eq 'ConsoleHost') {
-        exit
-    }
-    Write-Warning 'Only usable while in the PowerShell console host'
-}
-
-Set-Alias -Name 'reload' -Value 'Restart-PowerShell'
-
-$parentProcessId = (Get-WmiObject Win32_Process -Filter "ProcessId=$PID").ParentProcessId
-$parentProcessName = (Get-WmiObject Win32_Process -Filter "ProcessId=$parentProcessId").ProcessName
-
-if ($host.Name -eq 'ConsoleHost') {
-    if (-not($parentProcessName -eq 'powershell.exe')) {
-        Invoke-PowerShell
-    }
-}
